77d72fe54411cf35cd1d6b1bd144e52b5fe8f4ac
Enzo <e.valfort@epastelsoft.com>
2023-07-18 14:04:36 +0200

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const proxy = require("express-http-proxy")
 
-app.use("/api/auth", proxy("http://localhost:8081"))
+app.use("/api/auth", proxy("http://2-authentication-service-1:8081"));
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
